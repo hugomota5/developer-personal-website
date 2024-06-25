002c453c323b253ce492a0fb58288d76e4c38a11
hugomota5 <hugojcmota5@gmail.com>
2024-06-25 15:52:43 -0300

    navegação "mobile first"

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+// SHOW MENU
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
+
+// VALIDATE
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("nav__menu-open");
+  });
+}
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("nav__menu-open");
+  });
+}
+// REMOVE MENU MOBILE
+const navLink = document.querySelectorAll(".nav__link");
+const linkAction = () => {
+  const navMenu = document.getElementById("nav-menu");
+};
+navLink.forEach((n) => n.addEventListener("click", linkAction));
